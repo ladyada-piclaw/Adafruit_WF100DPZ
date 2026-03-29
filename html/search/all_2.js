a812613acf3024a987cf3443e7982395289ef7b6
Doxygen CI <ci-arduino@invalid>
2026-03-29 20:04:33 +0000

    Deploy docs to GitHub Pages from commit 6b48a64c40
    Commit: 6b48a64c408501ec68b24c34af6d29807da6dff9
    GitHub Actions run: 23717805709

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['getmaxpressure',['getMaxPressure',['../class_adafruit___w_f100_d_p_z.html#aff2addf120231116fca7472463e03356',1,'Adafruit_WF100DPZ']]],
   ['getmeasurementmode',['getMeasurementMode',['../class_adafruit___w_f100_d_p_z.html#ab295afa241d4ffccf2494a207e344aae',1,'Adafruit_WF100DPZ']]],
   ['getpartid',['getPartID',['../class_adafruit___w_f100_d_p_z.html#ad43f3708d820667a0c1cdcf61ed3b207',1,'Adafruit_WF100DPZ']]],
   ['getsleepinterval',['getSleepInterval',['../class_adafruit___w_f100_d_p_z.html#a0ab040b44bad14c519a9b4b5864cb005',1,'Adafruit_WF100DPZ']]],
